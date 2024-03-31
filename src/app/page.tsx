fe0de52b4d9e85a475ca82251a91279e0c69a443
Rashed <rashedulhoqueceo@gmail.com>
2024-03-31 14:56:19 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,85 @@
 import Image from "next/image";
+import player1 from "../assets/player1.jpg"
+import player2 from "../assets/player2.jpg"
+import player3 from "../assets/player3.jpg"
+import player4 from "../assets/player4.avif"
+import add from "../assets/add.jpeg"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-full min-h-screen mx-auto bg-[#F7F7F8] dark:bg-[#292B32] px-12 py-8">
+      <div className="max-w-[1445px] mx-auto">
+        <h4 className="bg-none text-[#000000] dark:text-[#ffffff] text-3xl font-extrabold border-b-4 w-fit py-2 border-[#738FFF]">Sports</h4>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-6">
+          <div className="bg-[#ffffff] dark:bg-[#3B3E47] p-3 shadow-md">
+            <Image src={player1} alt="player1" className="w-[220px] h-[350px] mx-auto"/>
+            <h4 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff] mb-2 mt-2">Sacramento River Cats</h4>
+            <div className="flex justify-between items-center p-2 bg-[#F7F7F8] dark:bg-[#292B32]">
+              <div>
+                <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Total Events</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">48 Events</p>
+              </div>
+              <div>
+              <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Sport</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">Baseball</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#ffffff] dark:bg-[#3B3E47] p-3 shadow-md">
+            <Image src={player2} alt="player1" className="w-[220px] h-[350px] mx-auto"/>
+            <h4 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff] mb-2 mt-2">Las Vegas Aviators</h4>
+            <div className="flex justify-between items-center p-2 bg-[#F7F7F8] dark:bg-[#292B32]">
+              <div>
+                <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Total Events</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">28 Events</p>
+              </div>
+              <div>
+              <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Sport</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">Baseball</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#ffffff] dark:bg-[#3B3E47] p-3 shadow-md">
+            <Image src={player4} alt="player1" className="w-[220px] h-[350px] mx-auto"/>
+            <h4 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff] mb-2 mt-2">New Jersey Devils</h4>
+            <div className="flex justify-between items-center p-2 bg-[#F7F7F8] dark:bg-[#292B32]">
+              <div>
+                <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Total Events</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">15 Events</p>
+              </div>
+              <div>
+              <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Sport</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">Ice Hockey</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#ffffff] dark:bg-[#3B3E47] p-3 shadow-md">
+            <Image src={player3} alt="player1" className="w-[220px] h-[350px] mx-auto"/>
+            <h4 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff] mb-2 mt-2">Los Angeles Dodgers</h4>
+            <div className="flex justify-between items-center p-2 bg-[#F7F7F8] dark:bg-[#292B32]">
+              <div>
+                <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Total Events</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">38 Events</p>
+              </div>
+              <div>
+              <h4 className="text-sm font-normal text-[#525965] dark:text-[#DFDFDF]">Sport</h4>
+                <p className="text-base font-medium text-[#000000] dark:text-[#ffffff]">Baseball</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#ffffff] dark:bg-[#3B3E47] p-3 shadow-md">
+          <div className="border border-[#006555] h-full relative">
+           <Image src={add} alt="player1" className="w-[220px] h-[220px] mx-auto"/>
+           <button className="absolute top-0 right-0 py-1 px-3 bg-[#000000] text-[#ffffff] text-xs font-bold">
+            Ad
+           </button>
+           <div className="px-2 py-6">
+           <h4 className="text-[#222D3A] dark:text-[#ffffff] text-xl font-semibold mb-2">ProGear: Peak Performance Awaits!</h4>
+           <p className="text-[#525965] dark:text-[#DFDFDF] text-sm">Experience peak performance with ProGear! Elevate your game with our premium sports equipment and apparel. Dominate the competition and unleash your full potential with ProGear by your side!</p>
+           </div>
+          </div>   
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
